d99fdc24734561040a537600156f4b7b82b240a0
veljkopopovic <veljko@888.rs>
2014-01-03 10:13:03 +0100

    stupid bug with text/tspans relation resolved

---
+++ src/components/wheel_group.js
@@ -0,0 +1,9 @@
+(function (global) {
+  var fabric = global.fabric || (global.fabric = { }),
+      extend = fabric.util.object.extend;
+
+	fabric.WheelGroup = function (svgelem, config) {
+		console.log('===', svgelem.id);
+	}
+
+})(typeof exports !== 'undefined' ? exports : this);
